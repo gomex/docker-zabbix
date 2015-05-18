c691d7dff25063bfcf5a0fae992fe88f441b789e
George Moura <gwmoura@gmail.com>
2015-05-18 15:50:05 -0300

    add tests, but I am not implemented

---
+++ tests/test_docker_stats.py
@@ -0,0 +1,17 @@
+import unittest
+
+import docker_service
+
+class TestDockerService(unittest.TestCase):
+
+	def test_list_containers(self):
+		self.assertTrue(True)
+
+	def test_docker_stats(self):
+		self.assertTrue(True)
+
+	def test_calculate_cpu_percent(self):
+		self.assertTrue(True)
+
+	def test_calculate_upercent_used_memory(self):
+		self.assertTrue(True)
