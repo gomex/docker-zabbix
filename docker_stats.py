734d0f73d58a0f2d209d41dd7f4401dddc6bb990
Gomex <gomex@riseup.net>
2015-05-20 00:38:41 -0300

    Fixed PR of George

--- docker_stats.py
+++ docker_stats.py
@@ -1,13 +1,13 @@
 from docker_service import DockerService
 
-parser = OptionParser()
+parser = DockerService.OptionParser()
 parser.add_option('-u', '--url', default='unix://var/run/docker.sock',
                   help='URL for Docker service (Unix or TCP socket).')
 parser.add_option('-l', action="store_true", dest="list", default=False)
 (opts, args) = parser.parse_args()
 
 # Docker access
-docker_service = DockerService(opts.url)
+docker_service = DockerService.DockerService(opts.url)
 containerslist = docker_service.list_containers()
 if opts.list == True :
     first = 1;
@@ -31,20 +31,20 @@ else :
         stats = docker_service.docker_stats(container['Id'])
         previousCPU = stats['cpu_stats']['cpu_usage']['total_usage']
         previousSystem = stats['cpu_stats']['system_cpu_usage']
-        CPUPercent = docker_service.calculateCPUPercent(previousCPU, previousSystem, container)
-        PercentMemoryUsed = docker_service.calculateupercentusedmemory(container)
+        CPUPercent = docker_service.calculate_cpu_percent(previousCPU, previousSystem, container)
+        PercentMemoryUsed = docker_service.calculate_upercent_used_memory(container)
 
         packet = [
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[cpu_used_percent]', round(CPUPercent, 2)),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[memory_used_percent]', PercentMemoryUsed),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[bytes_received]', stats['network']['rx_bytes']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[bytes_sent]', stats['network']['tx_bytes']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_received]', stats['network']['rx_packets']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[packets sent]', stats['network']['tx_packets']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_received_dropped]', stats['network']['rx_dropped']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_sent_dropped]', stats['network']['tx_dropped']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_received_erros]', stats['network']['rx_errors']),
-             ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_sent_erros]', stats['network']['tx_errors']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[cpu_used_percent]', round(CPUPercent, 2)),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[memory_used_percent]', PercentMemoryUsed),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[bytes_received]', stats['network']['rx_bytes']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[bytes_sent]', stats['network']['tx_bytes']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_received]', stats['network']['rx_packets']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[packets sent]', stats['network']['tx_packets']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_received_dropped]', stats['network']['rx_dropped']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_sent_dropped]', stats['network']['tx_dropped']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_received_erros]', stats['network']['rx_errors']),
+             DockerService.ZabbixMetric(str(Name)[4:-2], 'user.docker[packets_sent_erros]', stats['network']['tx_errors']),
         ]
 
-        result = ZabbixSender(use_config=True).send(packet)
+        result = DockerService.ZabbixSender(use_config=True).send(packet)
